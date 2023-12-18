38c271c6a28ce00639184a13454cfd0389b5ecbd
nguyentienlam <ntl260303@gmail.com>
2023-12-18 22:34:13 +0700

    [start]: signin signup

---
+++ src/middlewares/checkPermission.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import User from "../models/User";
+dotenv.config();
+
+const { SECRET_CODE } = process.env;
+export const checkPermission = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(403).json({
+        message: "Bạn chưa đăng nhập!",
+      });
+    }
+    const decoded = jwt.verify(token, SECRET_CODE);
+    if (!decoded) {
+      throw new Error("Token Error!");
+    }
+    const user = await User.findById(decoded._id);
+    if (!user) {
+      return res.status(404).json({
+        message: "User không tồn tại trong hệ thống!",
+      });
+    }
+    if (user.role !== "admin") {
+      return res.status(400).json({
+        message: "Bạn không có quyền làm việc này!",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      name: error.name,
+      message: error.message,
+    });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,7 @@
 import express from "express";
+import routerAuth from "./auth";
+
 const router = express.Router();
+router.use("/auth", routerAuth);
+
 export default router;
