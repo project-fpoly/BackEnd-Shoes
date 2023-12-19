5c55beec8c716bc55f4517a9aaff618ffb41b90c
Lam Nguyen <ntl03.dev@gmail.com>
2023-12-19 15:00:24 +0700

    [user]:update

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,13 @@
 import { Router } from "express"
 import { deleteUser, getAllUsers, signIn, signUp, updateUser } from "../controllers/auth"
+import { checkPermission } from "../middlewares/checkPermission"
 
 const routerAuth = Router()
 
 routerAuth.post("/signup", signUp)
 routerAuth.post("/signin", signIn)
 routerAuth.get("/users", getAllUsers);
-routerAuth.delete("/users/:userId", deleteUser);
-routerAuth.put("/users/:userId", updateUser);
+routerAuth.put("/users/:userId",updateUser);
+//chỉ admin mới có quyền xoá
+routerAuth.delete("/users/:userId",checkPermission, deleteUser);
 export default routerAuth
