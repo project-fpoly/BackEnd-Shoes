3cba58afc979aaf2e3d9725b723664f7233ef5fd
dong959 <binz2028@gmail.com>
2023-12-20 15:01:36 +0700

    update route cart

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import routerAuth from "./auth";
+import routerCart from "./cart";
 
 const router = express.Router();
 router.use("/auth", routerAuth);
+router.use("/order", routerCart);
 export default router;
