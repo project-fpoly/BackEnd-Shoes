8e78446617090903bc42fdce7468065166a33ba1
dong959 <binz2028@gmail.com>
2023-12-25 20:40:17 +0700

    edit

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -28,7 +28,7 @@ const cartSchema = mongoose.Schema(
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     isPaid: { type: Boolean, default: false },
     paidAt: { type: Date },
-    isDelivered: { type: Boolean, default: false },
+    isDelivered: { type: String, default: "Đang xét duyệt" },
     deliveredAt: { type: Date },
   },
   {
