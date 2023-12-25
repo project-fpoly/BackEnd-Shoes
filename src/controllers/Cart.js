0439b1d457a4aebc3367f02a4aad121e35c79dd3
dong959 <binz2028@gmail.com>
2023-12-25 21:39:11 +0700

    validate cart

--- src/controllers/Cart.js
+++ src/controllers/Cart.js
@@ -1,5 +1,5 @@
 import Cart from "../models/Cart";
-
+import { cartSchema } from "../validations/cart";
 const createCart = async (req, res) => {
   try {
     const {
@@ -11,8 +11,7 @@ const createCart = async (req, res) => {
       totalPrice,
       user,
     } = req.body;
-
-    const newCart = new Cart({
+    const { error, value } = cartSchema.validate({
       cartItem,
       shippingAddress,
       paymentMethod,
@@ -21,6 +20,12 @@ const createCart = async (req, res) => {
       totalPrice,
       user,
     });
+    if (error) {
+      return res.status(400).json({
+        message: error.message,
+      });
+    }
+    const newCart = new Cart(value);
 
     const savedCart = await newCart.save();
 
@@ -34,9 +39,27 @@ const createCart = async (req, res) => {
 };
 const getAllCarts = async (req, res) => {
   try {
-    const carts = await Cart.find();
+    const page = parseInt(req.query.page) || 1; // Trang hiện tại, mặc định là 1
+    const limit = parseInt(req.query.limit) || 10; // Giới hạn số lượng bản ghi trên mỗi trang, mặc định là 10
 
-    return res.status(200).json(carts);
+    const skipCount = (page - 1) * limit; // Số bản ghi cần bỏ qua
+
+    const totalCount = await Cart.countDocuments(); // Tổng số bản ghi trong collection
+
+    const carts = await Cart.find().skip(skipCount).limit(limit).exec();
+
+    if (carts.length === 0) {
+      return res.status(404).json({
+        message: "Không tìm thấy giỏ hàng nào",
+      });
+    }
+
+    return res.status(200).json({
+      message: `Tìm thấy ${carts.length} giỏ hàng`,
+      carts: carts,
+      currentPage: page,
+      totalPages: Math.ceil(totalCount / limit),
+    });
   } catch (e) {
     return res.status(500).json({
       message: "Lỗi máy chủ",
@@ -70,7 +93,15 @@ const updateCart = async (req, res) => {
     const cartId = req.params.id;
     const updateData = req.body;
 
-    const updatedCart = await Cart.findByIdAndUpdate(cartId, updateData, {
+    const { error, value } = cartSchema.validate(updateData);
+
+    if (error) {
+      return res.status(400).json({
+        message: error.details[0].message,
+      });
+    }
+
+    const updatedCart = await Cart.findByIdAndUpdate(cartId, value, {
       new: true,
     });
 
--- src/models/User.js
+++ src/models/User.js
@@ -7,13 +7,8 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     email: {
-<<<<<<< HEAD
       type: String,
       required: true,
-=======
-        type: String,
-        required: true
->>>>>>> da9b94690d6446b27254ff3856f25d7000424232
     },
     password: {
       type: String,
@@ -38,16 +33,10 @@ const userSchema = new mongoose.Schema(
       type: String,
     },
     phoneNumbers: {
-<<<<<<< HEAD
       type: Array,
     },
   },
   { versionKey: false, timestamps: true }
 );
-=======
-        type: Array
-    }
-}, { versionKey: false, timestamps: true })
->>>>>>> da9b94690d6446b27254ff3856f25d7000424232
 
 export default mongoose.model("User", userSchema);
