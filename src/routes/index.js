89f8a37f64ccc04f296edb80066f3858c7544566
nguyentienlam <ntl260303@gmail.com>
2023-12-25 21:52:00 +0700

    [merge]:update bill product category

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import routerAuth from "./auth";
 import routerCategory from "./category";
 import routerProduct from "./product";
+import routerBill from "./bill";
 
 const router = express.Router();
 router.use("/auth", routerAuth);
-
+router.use("/bill", routerBill);
+router.use("/product", routerProduct);
 router.use("/categories", routerCategory);
 
 export default router;
