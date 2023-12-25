f6e299a4015e879f3cafcf4c1e4e7d2c4e5db58e
thanh <miktenthanh@gmail.com>
2023-12-25 08:57:50 +0700

    f

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,7 @@
 import express from "express";
 import routerAuth from "./auth";
-<<<<<<< HEAD
 import routerBill from "./bill";
-=======
 import routerCategory from "./category";
->>>>>>> da9b94690d6446b27254ff3856f25d7000424232
 
 const router = express.Router();
 router.use("/auth", routerAuth);
