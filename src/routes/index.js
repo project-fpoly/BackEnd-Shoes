7559d6fc9b46f574c0fcbc2818e12b5f80df64dc
Lam Nguyen <ntl03.dev@gmail.com>
2024-01-26 09:41:00 +0700

    [LAM]:fix import routerCategory

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import routerAuth from "./auth";
 import routerBill from "./bill";
+import routerCategory from "./category";
 
 const router = express.Router();
 router.use("/auth", routerAuth);
