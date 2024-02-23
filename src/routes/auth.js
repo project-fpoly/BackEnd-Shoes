8462e95e1980304c475013bcde1a3a914d9b8a5f
ntrkien001 <ntrkien001@gmail.com>
2024-02-23 12:51:41 +0700

    fix cat

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import {
   createUser,
   deleteMoreUsers,
-  deleteUser,
+  
   forgotPassword,
   getAllUsers,
   getOneUser,
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { createCategory, getAllCategory, removeCategory, updateCategory } from "../controllers/category"
+import { createCategory, getAllCategory, removeCategory, updateCategory,getOneCategory } from "../controllers/category"
 
 const routerCategory = Router()
 
 routerCategory.get("/", getAllCategory);
+routerCategory.get("/:id", getOneCategory);
 routerCategory.post("/", createCategory);
 routerCategory.put(`/:id`, updateCategory);
 routerCategory.delete("/:id", removeCategory);
