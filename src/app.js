670443031fa3e6e514a75f704d6cf57fe67f2102
Lam Nguyen <ntl03.dev@gmail.com>
2024-03-07 17:07:14 +0700

    [DF-73]: test new Notification when call

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 import express from "express";
+import http from "http"; // Import the 'http' module
+import { Server as SocketIOServer } from "socket.io";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
 import mongoose from "mongoose";
 import cors from "cors";
 import session from "express-session";
-const app = express();
-
+import { createNotificationForAdmin } from "./controllers/notification.js"; 
 dotenv.config();
 const { PORT, DB_URI, SECRET_CODE } = process.env;
 
+const app = express();
+const server = http.createServer(app); 
+const io = new SocketIOServer(server); 
+
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
 app.use(
   session({
-    secret: SECRET_CODE, // Thay thế bằng một chuỗi bí mật mạnh hơn trong môi trường thực tế
+    secret: SECRET_CODE,
     resave: false,
     saveUninitialized: true,
   })
@@ -23,8 +27,22 @@ app.use(
 mongoose.connect(DB_URI).then(() => {
   console.log("Connected!");
 });
+
 app.use("/api", router);
 
-app.listen(PORT, () => {
+// Socket.io implementation
+io.on("connection", (socket) => {
+  socket.on("new_user_login", (data) => {
+    io.emit("new_user_login", { message: data.message });
+  });
+  socket.on("newNotification",  (data) => {
+      io.emit("newNotification", { message: data.message });
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
