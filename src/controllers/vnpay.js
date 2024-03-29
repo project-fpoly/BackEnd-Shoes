d8df367ad453016978cbb5ea8b02272c714915c3
dong959 <binz2028@gmail.com>
2024-03-29 14:42:18 +0700

    update vnpay

--- src/controllers/vnpay.js
+++ src/controllers/vnpay.js
@@ -28,8 +28,8 @@ router.post("/create_payment_url", (req, res, next) => {
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress;
-  let tmnCode = vnp_TmnCode;
-  let secretKey = vnp_HashSecret;
+  let tmnCode = "RLE42FCR";
+  let secretKey = "OQPUUZRVSSJASOQVUQHHURHBXGDIMBTU";
   let vnpUrl = vnp_Url;
   let returnUrl = vnp_Returnurl;
   const date = new Date();
